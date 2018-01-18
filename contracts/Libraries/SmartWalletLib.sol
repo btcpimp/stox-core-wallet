56c741492a720094bc6f2c0bfeb93c2e87faed33
Danny <danny@stox.com>
2018-01-18 13:59:50 +0200

    Added new Smart wallet using a library

---
+++ contracts/Libraries/SmartWalletLib.sol
@@ -0,0 +1,111 @@
+pragma solidity ^0.4.19;
+import "../token/IERC20Token.sol";
+
+///@title - a contract that represents a smart wallet, created by Stox, for every new Stox user
+library SmartWalletLib {
+
+    /*
+     *  Structs
+     */
+     struct Wallet {
+        address operatorAccount;
+        address backupAccount;
+        address userWithdrawalAccount;
+    }
+
+    /*
+     *  Members
+     */
+    string constant VERSION = "0.1";
+   
+
+    /*
+     *  Modifiers
+     */
+    modifier validAddress(address _address) {
+        require(_address != 0x0);
+        _;
+    }
+
+    modifier addressNotSet(address _address) {
+        require(_address == 0);
+        _;
+    }
+
+    modifier operatorOnly(address _operatorAccount) {
+        require(msg.sender == _operatorAccount);
+        _;
+    }
+
+    /*
+     *  Events
+     */
+    event TransferToBackupAccount(address _token, address _backupAccount, uint _amount);
+    event TransferToUserWithdrawalAccount(address _token, address _userWithdrawalAccount, uint _amount);
+    event SetUserWithdrawalAccount(address _userWithdrawalAccount);
+
+    /*
+        @dev Initialize the wallet with the operator and backupAccount address
+        
+        @param _userWithdrawalAccount       The user account to withdraw funds to
+        
+    */
+    function initWallet(Wallet storage _self, address _backupAccount, address _operator) 
+            public
+            validAddress(_backupAccount)
+            validAddress(_operator)
+            {
+        
+        _self.operatorAccount = _backupAccount;
+        _self.backupAccount = _operator;
+    }
+
+    /*
+        @dev Setting the account of the user to send funds to. 
+        
+        @param _userWithdrawalAccount       The user account to withdraw funds to
+        
+    */
+    function setUserWithdrawalAccount(Wallet storage _self, address _userWithdrawalAccount) 
+            public
+            operatorOnly(_self.operatorAccount)
+            validAddress(_userWithdrawalAccount)
+            addressNotSet(_self.userWithdrawalAccount)
+            {
+        
+        _self.userWithdrawalAccount = _userWithdrawalAccount;
+        SetUserWithdrawalAccount(_userWithdrawalAccount);
+    }
+
+    /*
+        @dev Withdraw funds to a backup account. 
+
+
+        @param _token               The ERC20 token the owner withdraws from 
+        @param _amount              Amount to transfer    
+    */
+    function transferToBackupAccount(Wallet storage _self, IERC20Token _token, uint _amount) 
+            public 
+            operatorOnly(_self.operatorAccount)
+            {
+
+        _token.transfer(_self.backupAccount, _amount);
+        TransferToBackupAccount(_token, _self.backupAccount, _amount); 
+    }
+
+    /*
+        @dev Withdraw funds to the user account. 
+
+        @param _token               The ERC20 token the owner withdraws from 
+        @param _amount              Amount to transfer    
+    */
+    function transferToUserWithdrawalAccount(Wallet storage _self, IERC20Token _token, uint _amount) 
+            public 
+            operatorOnly(_self.operatorAccount)
+            validAddress(_self.userWithdrawalAccount)
+            {
+
+        _token.transfer(_self.userWithdrawalAccount, _amount);
+        TransferToUserWithdrawalAccount(_token, _self.userWithdrawalAccount, _amount);   
+    }
+}
---
+++ contracts/SmartWallet/SmartWallet.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.4.19;
+import "../token/IERC20Token.sol";
+import "../Libraries/SmartWalletLib.sol";
+
+
+///@title - a contract that represents a smart wallet, created by Stox, for every new Stox user
+contract SmartWallet {
+
+    /*
+     *  Members
+     */
+    using SmartWalletLib for SmartWalletLib.Wallet;
+    SmartWalletLib.Wallet public wallet;
+
+    /*
+     *  Events
+     */
+    event TransferToBackupAccount(address _token, address _backupAccount, uint _amount);
+    event TransferToUserWithdrawalAccount(address _token, address _userWithdrawalAccount, uint _amount);
+    event SetUserWithdrawalAccount(address _userWithdrawalAccount);
+     
+    /*
+        @dev constructor
+
+        @param _backupAccount       A default operator's account to send funds to, in cases where the user account is
+                                    unavailable or lost
+        @param _operator            The contract operator address 
+
+    */
+    function SmartWallet(address _backupAccount, address _operator) public {
+        wallet.initWallet(_backupAccount, _operator);
+    }
+
+    /*
+        @dev Setting the account of the user to send funds to. 
+        
+        @param _userWithdrawalAccount       The user account to withdraw funds to
+        
+    */
+    function setUserWithdrawalAccount(address _userWithdrawalAccount) public {
+        wallet.setUserWithdrawalAccount(_userWithdrawalAccount);
+    }
+
+    /*
+        @dev Withdraw funds to a backup account. 
+
+
+        @param _token               The ERC20 token the owner withdraws from 
+        @param _amount              Amount to transfer    
+    */
+    function transferToBackupAccount(IERC20Token _token, uint _amount) public {
+        wallet.transferToBackupAccount(_token, _amount);
+    }
+
+    /*
+        @dev Withdraw funds to the user account. 
+
+
+        @param _token               The ERC20 token the owner withdraws from 
+        @param _amount              Amount to transfer    
+    */
+    function transferToUserWithdrawalAccount(IERC20Token _token, uint _amount) public {
+        wallet.transferToUserWithdrawalAccount(_token, _amount);
+    }
+}
